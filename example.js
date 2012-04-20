1033c4ba4b4b7412dab48ff4c0777b8674e9cabe
Steve Engledow <steve@offend.me.uk>
2012-04-20 11:56:01 +0100

    Add example code

---
+++ example.js
@@ -0,0 +1,23 @@
+var nosef = require("nosef");
+
+var echo_handler = nosef.handler(function(request, response, params) {
+    response.JSON(params);
+});
+
+var hello_handler = nosef.handler(function(request, response, params) {
+    response.end("Hello " + (params.who || "world"));
+});
+
+var config = {
+    port: 8765,
+    urls: [
+        ["/echo/{{path}}", echo_handler],
+        ["/hello/{who}", hello_handler]
+    ]
+};
+
+nosef.server.start(config, function() {
+    console.log("Server stopped");
+});
+
+console.log("HTTP server started on port " + config.port);
