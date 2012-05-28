027f7e90300afb987c2856db169cca288c3fae41
Steve Engledow <steve@offend.me.uk>
2012-05-28 16:39:51 +0100

    Add file_handler helper function

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-exports.handler = require("./lib/handler");
+var handlers = require("./lib/handlers");
+
+exports.handler = handlers.handler;
+exports.file_handler = handlers.file_handler;
+
 exports.server = require("./lib/server");
